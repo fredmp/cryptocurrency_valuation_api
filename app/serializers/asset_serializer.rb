fbb550297f4c95678fe92f3aeea4833eb9e9300e
Frederico Martins Porto <martinsporto@gmail.com>
2018-01-26 20:51:32 -0200

    Work in progress - assets

--- app/serializers/asset_serializer.rb
+++ app/serializers/asset_serializer.rb
@@ -1,4 +1,13 @@
 class AssetSerializer < ActiveModel::Serializer
   attributes :id, :amount, :btc_value, :usd_value
   belongs_to :currency, serializer: CurrencySerializer
+
+  def btc_value
+    currency = object.currency
+    (object.amount * (currency.price / currency.reference_price)).to_f.round(8)
+  end
+
+  def usd_value
+    (object.amount * object.currency.price).to_f.round(4)
+  end
 end
