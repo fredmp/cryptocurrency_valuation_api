7c96f29755b78cb3d2dc665cc9197f2476072564
Frederico Martins Porto <martinsporto@gmail.com>
2018-02-04 16:04:55 -0200

    Add tests to valuations controller

--- spec/controllers/valuations_controller_spec.rb
+++ spec/controllers/valuations_controller_spec.rb
@@ -2,6 +2,29 @@ require 'rails_helper'
 
 RSpec.describe ValuationsController, type: :controller do
 
-  pending "GET #update"
+  let(:wallet_valuation) { create(:wallet_valuation) }
+
+  before(:each) do
+    allow(controller).to receive(:current_user) { wallet_valuation.tracked_currency.user }
+  end
+
+  describe 'GET #update' do
+    it 'updates a valuation' do
+      post :update, params: { id: wallet_valuation.id, value: 5 }
+
+      expect(response).to have_http_status(:success)
+      expect(response.body).to eq({
+        "id": wallet_valuation.id,
+        "value": 5,
+        "valuationSetting": {
+          "id": wallet_valuation.valuation_setting.id,
+          "name": "Innovation",
+          "description": nil,
+          "maxValue": 10,
+          "weight": "40.0"
+        }
+      }.to_json)
+    end
+  end
 
 end
--- spec/factories/valuations.rb
+++ spec/factories/valuations.rb
@@ -1,6 +1,9 @@
 FactoryBot.define do
   factory :valuation do
 
+    tracked_currency
+    association :valuation_setting, factory: :innovation
+
     factory :innovation_valuation do
       value 8
     end
