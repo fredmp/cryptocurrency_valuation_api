f9a42bcec9c8906dd4906f91553310b2247a745e
Frederico Martins Porto <martinsporto@gmail.com>
2018-03-24 19:36:20 -0300

    Add domain to cors allowed origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'cryptocurrency-valuation.herokuapp.com', 'localhost:8080'
+    origins 'cryptocurrency-valuation.cosmostecnologia.com.br', 'cryptocurrency-valuation.herokuapp.com', 'localhost:8080'
 
     resource '*',
       headers: :any,
